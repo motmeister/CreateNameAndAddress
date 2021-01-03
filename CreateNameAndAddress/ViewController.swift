974b15af9aafb0259cb6b6cf72f8da4cee69a686
Dennis Schaefer <motmeister@gmail.com>
2021-01-02 17:48:45 -0800

    Initial Commit

---
+++ CreateNameAndAddress/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CreateNameAndAddress
+//
+//  Created by Dennis Schaefer on 1/2/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
